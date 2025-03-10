59306c80dda359f5855f73f7d3672c768b57889c
MathieuAudibert <mathieu.audibert27@gmail.com>
2025-03-10 11:24:56 +0100

    ✨ feat: ajout de la connexion a firebase

---
+++ bankwerk/src/config/firebase.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+import { initializeApp } from "firebase/app";
+
+dotenv.config();
+
+const firebaseConfig = {
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  databaseURL: process.env.DATABASEURL,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID
+};
+
+const bankwerk = initializeApp(firebaseConfig);
