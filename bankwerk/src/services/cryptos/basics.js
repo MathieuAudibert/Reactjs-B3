a05be695865935db5ec335aa492af3bce15e64aa
MathieuAudibert <mathieu.audibert27@gmail.com>
2025-03-11 11:54:44 +0100

    🐞 fix: topCryptos recupere les bon ranks

--- bankwerk/src/services/cryptos/basics.js
+++ bankwerk/src/services/cryptos/basics.js
@@ -10,16 +10,42 @@ const topCryptos = async () => {
      * Args : 
      * 
      * Returns : 
-     * liste (dict)
+     * data (dict) : liste des cryptos
      */
     try {
-        res = await axios.get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=10&CMC_PRO_API_KEY=${api_key}`)
-        return res.data.data
+        res = await axios.get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=10&CMC_PRO_API_KEY=${api_key}&sort=cmc_rank`)
+        return { data: res.data.data, error: null}
     } catch (error) {
         console.error(error) 
+        return { data: null, error: error.message }
     }
 }
 
-const cryptoInfos = async (symbol) => {
-    
+const cryptoInfosParSymbol = async (symbol) => {
+    /** Renvoie les informations d'une crypto en fonction de son symbole
+     * 
+     * Args :
+     * symbol (str) : abréviation du nom de la crypto utilisé pour les transactions
+     * 
+     * Returns : 
+     * infos (dict) : infos de la crypto
+     */
+    try {  
+        res = await axios.get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/airdrops?symbol=${symbol}&CMC_PRO_API_KEY=${api_key}`)
+        return { data: res.data.data, error: null }
+    } catch (error) {
+        console.error(error)
+        return { data: null, error: error.message }
+    }
+}
+
+const cryptoInfosParNom = async (nom) => {
+    /** Renvoie les informations d'une crypto en fonction de son nom
+     * 
+     * Args : 
+     * nom (str)
+     * 
+     * Returns : 
+     * infos (dict) : infos de la crypto
+     */
 }
