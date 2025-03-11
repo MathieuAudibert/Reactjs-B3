23b5ed355e59a79881fdd3fe16bda7956f583b8f
MathieuAudibert <mathieu.audibert27@gmail.com>
2025-03-11 15:38:27 +0100

    🐞 fix: no dupplicates

--- bankwerk/src/services/cryptos/cryptoCollection.js
+++ bankwerk/src/services/cryptos/cryptoCollection.js
@@ -1,14 +1,14 @@
-const { auth, db } = require('../../config/firebase.js')
-const { topCryptos, cryptoInfosParSymbol } = require('./basics.js')
+const { auth, db } = require('../../config/firebase.js');
+const { topCryptos, cryptoInfosParSymbol } = require('./basics.js');
 
-cryptoCollection = db.collection('Crypto')
+const cryptoCollection = db.collection('Crypto');
 
 async function updateCryptoCollection() {
-    const cryptos = await topCryptos()
+    const cryptos = await topCryptos();
 
     for (const cry of cryptos.data) {
-        const specific_data = await cryptoInfosParSymbol(cry.symbol)
-        const quoteEUR = specific_data.data[cry.symbol].quote.EUR
+        const specific_data = await cryptoInfosParSymbol(cry.symbol);
+        const quoteEUR = specific_data.data[cry.symbol].quote.EUR;
 
         const data = {
             rang: cry.rank,
@@ -17,21 +17,26 @@ async function updateCryptoCollection() {
             pourcent_30j: quoteEUR.percent_change_30d,
             prix: quoteEUR.price,
             derniere_update: new Date()
-        }
+        };
 
-        const cryptoDocs = await cryptoCollection.where('symbol', '==', cry.symbol).get()
+        const cryptoDocs = await cryptoCollection.where('symbole', '==', cry.symbol).get();
 
         if (!cryptoDocs.empty) {
-            const cryptoDoc = cryptoDocs.docs[0]
-            await cryptoDoc.ref.update(data)
+            const cryptoDoc = cryptoDocs.docs[0];
+            await db.runTransaction(async (t) => {
+                const doc = await t.get(cryptoDoc.ref);
+                if (doc.exists) {
+                    t.update(cryptoDoc.ref, data);
+                }
+            });
         } else {
-            await cryptoCollection.add(data)
+            await cryptoCollection.add(data);
         }
     }
 
-    console.log('Collection mise à jour')
+    console.log('Collection mise à jour');
 }
 
-setInterval(updateCryptoCollection, 15 * 60 * 1000) // 15min pr chaque update
+setInterval(updateCryptoCollection, 15 * 60 * 1000); // 15min pour chaque mise à jour
 
-updateCryptoCollection()
+updateCryptoCollection();
