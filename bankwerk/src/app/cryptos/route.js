3377df8bcd53d2b745b9a275a2cf4b96790ded5e
MathieuAudibert <mathieu.audibert27@gmail.com>
2025-03-11 16:26:58 +0100

    ✨ feat: affichage des cryptos

--- bankwerk/src/app/cryptos/route.js
+++ bankwerk/src/app/cryptos/route.js
@@ -1,7 +1,22 @@
-const { auth, db } = require('../../config/firebase.js')
-const { topCryptos, cryptoInfosParSymbol, cryptoInfosParNom } = require('./basics.js')
+const { auth, db } = require('../../config/firebase.js');
+const { topCryptos, cryptoInfosParSymbol, cryptoInfosParNom } = require('../../services/cryptos/basics.js');
 
 export async function GET(req) {
-    const cryptos = db.collection('Crypto').get();
-    
+    try {
+        const crypto = await db.collection('Crypto').get();
+        const cryptos = crypto.docs.map(doc => doc.data());
+        return new Response(JSON.stringify(cryptos), {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    } catch (error) {
+        console.error('Erreur lors de la récupération des cryptomonnaies:', error);
+        return new Response(JSON.stringify({ error: 'Erreur serveur' }), {
+            status: 500,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    }
 }
