3c68c328cf2471460d2720807db6c8e6261de30b
MathieuAudibert <mathieu.audibert27@gmail.com>
2025-04-24 20:28:25 +0200

    🦄 refactor: plus de config.json mais un seul gros dotenv

--- bankwerk/src/config/firebaseAdmin.js
+++ bankwerk/src/config/firebaseAdmin.js
@@ -1,29 +1,39 @@
-const admin = require("firebase-admin")
-const firebaseAccount = require("./config.json")
+const admin = require("firebase-admin");
+require('dotenv').config(); 
+
+const firebaseAccount = {
+  type: process.env.NEXT_PUBLIC_TYPE,
+  project_id: process.env.NEXT_PUBLIC_PROJECT_ID,
+  private_key_id: process.env.NEXT_PUBLIC_PRIVATE_KEY_ID,
+  private_key: process.env.NEXT_PUBLIC_PRIVATE_KEY.replace(/\\n/g, '\n'), 
+  client_email: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
+  client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+  auth_uri: process.env.NEXT_PUBLIC_AUTH_URI,
+  token_uri: process.env.NEXT_PUBLIC_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.NEXT_PUBLIC_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.NEXT_PUBLIC_CLIENT_X509_CERT_URL,
+};
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(firebaseAccount),
-  })
+  });
 }
 
-const auth = admin.auth()
-const db = admin.firestore()
-const FieldValue = admin.firestore.FieldValue
+const auth = admin.auth();
+const db = admin.firestore();
+const FieldValue = admin.firestore.FieldValue;
 
 async function verifyToken(req) {
   try {
-    const authHeader = req.headers.get("authorization")
-
-    const token = authHeader.split(" ")[1]
-    const decodedToken = await auth.verifyIdToken(token)
-    return decodedToken
+    const authHeader = req.headers.get("authorization");
+    const token = authHeader.split(" ")[1];
+    const decodedToken = await auth.verifyIdToken(token);
+    return decodedToken;
   } catch (error) {
-    console.error(error.message)
-    return null 
+    console.error(error.message);
+    return null;
   }
 }
 
-
-module.exports = { auth, db, verifyToken, FieldValue}
-
+module.exports = { auth, db, verifyToken, FieldValue };
